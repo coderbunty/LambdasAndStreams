30d08352c86a7509f6d603e6b0618de0fd657e72
Abhishek Sharma <abhipassion.sharma@gmail.com>
2021-05-31 18:57:01 +0530

    removing empty lines

--- src/com/wm/Streams.java
+++ src/com/wm/Streams.java
@@ -177,16 +177,6 @@ public class Streams {
                       .sum()
         );
 
-
-
-
-
-
-
-
-
-
-
         // Some exercises
         List<Integer> intNums = Arrays.asList(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
         // sum of twice of all the even numbers
